2b5755d04742cba537b19f44f98b3bde90fa741a
tutRPi <tutRPi@users.noreply.github.com>
2018-03-29 13:33:16 +0200

    Update MCP3008.py

--- MCP3008.py
+++ MCP3008.py
@@ -4,11 +4,11 @@ class MCP3008:
     def __init__(self, bus = 0, device = 0):
         self.bus, self.device = bus, device
         self.spi = SpiDev()
-        self.spi.max_speed_hz = 1000000 # 1MHz
         self.open()
 
     def open(self):
         self.spi.open(self.bus, self.device)
+        self.spi.max_speed_hz = 1000000 # 1MHz
     
     def read(self, channel = 0):
         cmd1 = 4 | 2 | (( channel & 4) >> 2)
